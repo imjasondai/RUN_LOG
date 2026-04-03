7267232e65f0230a6758aa576c4145718bbcc2ba
imjasondai <imjasondai@users.noreply.github.com>
2026-04-03 12:57:45 +0800

    Restore home overlay and force compact attribution

--- src/pages/RunDetail.tsx
+++ src/pages/RunDetail.tsx
@@ -57,6 +57,7 @@ const RunDetail = () => {
             setViewState={setViewState}
             changeYear={() => {}}
             thisYear={run.start_date_local.slice(0, 4)}
+            showActivityOverlay={false}
           />
         </div>
         <RunDetailPanel run={run} monthlyDistanceKm={monthlyDistanceKm} />
